f81b825ac5a3cfb1859080080e456dd8dc83008f
dedi27 <dedi27@gmail.com>
2018-06-12 10:42:56 -0300

    Create apps2.js

---
+++ apps2.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var express = require("express");
+var RED = require("node-red");
+
+
+// Create an Express app
+var app = express();
+
+// Add a simple route for static content served from 'public'
+app.use("/",express.static("public"));
+
+// Create a server
+var server = http.createServer(app);
+
+// Create the settings object - see default settings.js file for other options
+var settings = {
+    nodesDir: "/Users/allomov/work/altoros/node-red/app/nodes",
+    storageModule: require("node-red-flows-mongo"),
+    mongoUrl: 'mongodb://userXOU:FwsvooVFEWOO1sTf@mongodb:27017/nodered',
+    httpAdminRoot:"/",
+    httpNodeRoot: "/api",
+    uiPort: 8080,
+    functionGlobalContext: { }    // enables global context
+};
+
+// Initialise the runtime with a server and settings
+RED.init(server,settings);
+
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(8080);
+
+// Start the runtime
+RED.start();
